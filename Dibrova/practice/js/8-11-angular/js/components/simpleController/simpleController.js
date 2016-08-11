442077e4db2a70e74838a90c20c910e18ca15e74
Alex <a.laukhin@gmail.com>
2016-08-11 21:14:09 +0300

    11-08

---
+++ Dibrova/practice/js/8-11-angular/js/components/simpleController/simpleController.js
@@ -0,0 +1,27 @@
+myApp.controller('simpleController', function() {
+	var sCtrl = this;
+
+	sCtrl.table = [
+	{
+		id: 1,
+		name: 'test 1',
+		email: 'test1@gmail.com',
+		description: 'Some description',
+		isTrue: false
+	},
+	{
+		id: 2,
+		name: 'test 2',
+		email: 'test2@gmail.com',
+		description: 'Some description',
+		isTrue: false
+	},
+	{
+		id: 3,
+		name: 'test 3',
+		email: 'test3@gmail.com',
+		description: 'Some description',
+		Isworking: false
+	}
+	];
+})
