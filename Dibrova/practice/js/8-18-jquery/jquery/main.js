67a82af33061544a2e89b573c07e85e34ecbc03f
Alex <a.laukhin@gmail.com>
2016-08-18 21:33:41 +0300

    18-08

---
+++ Dibrova/practice/js/8-18-jquery/jquery/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	// $('h3').text('!!!');
+
+	// $('h3').css({
+	// 	"font-size": "40px",
+	// 	"color": "blue"
+	// })
+
+	// $('h3').css({
+	// 	color: "green",
+	// 	"font-weight": "bold"
+	// })
+
+	// $(':header')
+
+	// $('li').each(function(i, el) {
+	// 	console.log($('li'));
+	// 	$(this).text(i);
+	// })
+
+	// $('.test').css({
+	// 	color: "red"
+	// })
+
+	// $('P + P').css({
+	// 	color: "red"
+	// })
+
+	// $('li.www').next().css({
+	// 	color: "red"
+	// })
+
+	// $('h2 > i').css({
+	// 	color: "red"
+	// })
+
+	// $('li:not(.test)').css({
+	// 	color: "green"
+	// })
+
+	// $('li:contains(hello)').css({
+	// 	color: "green"
+	// })
+
+	$('h3').parents().find('i').css({
+		color: "green"
+	})
+
+
+
+});
